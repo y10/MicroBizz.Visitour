9513e2339e9e8572314b2d1724c28e47824aa0ff
ihor.shutiak <ihor.shutiak@solead.software>
2023-06-28 19:13:08 +0300

    Add project skeleton

---
+++ MicroBizz.Visitour/Soap/Models/Appointment.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace MicroBizz.Visitour.Soap.Models
+{
+    [DebuggerDisplay("{FunctionCode}; Status: {Status}; Info: {Info}")]
+    [DataContract(Name = nameof(Appointment), Namespace = "http://www.tourenserver.de/")]
+    public class Appointment
+    {
+        [DataMember(IsRequired = true)]
+        public int FunctionCode { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public int Status { get; set; }
+
+        [DataMember(IsRequired = true, Order = 2)]
+        public DateTime Date { get; set; }
+
+        [DataMember(IsRequired = true, Order = 3)]
+        public DateTime Time { get; set; }
+
+        [DataMember(IsRequired = true, Order = 4)]
+        public int Detour { get; set; }
+
+        [DataMember(Name = "FMVTID", IsRequired = true, EmitDefaultValue = false, Order = 5)]
+        public string? FmVtId { get; set; }
+
+        [DataMember(Name = "FMExtID", IsRequired = true, EmitDefaultValue = false, Order = 6)]
+        public string? FmExtId { get; set; }
+
+        [DataMember(IsRequired = true, EmitDefaultValue = false, Order = 7)]
+        public string? Info { get; set; }
+
+        [DataMember(IsRequired = true, Order = 8)]
+        public float Cost { get; set; }
+    }
+}
---
+++ MicroBizz.Visitour/Soap/Models/FixedCall.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace MicroBizz.Visitour.Soap.Models
+{
+    [DebuggerDisplay("{ExtId}; Info: {Info};")]
+    [DataContract(Name = nameof(FixedCall), Namespace = "http://www.tourenserver.de/")]
+    public class FixedCall
+    {
+        [DataMember(Name = "ExtID", IsRequired = true, EmitDefaultValue = false)]
+        public string? ExtId { get; set; }
+
+        [DataMember(Name = "VTID", IsRequired = true)]
+        public int VtId { get; set; }
+
+        [DataMember(IsRequired = true, Order = 2)]
+        public DateTime Date { get; set; }
+
+        [DataMember(IsRequired = true, Order = 3)]
+        public DateTime Arrival { get; set; }
+
+        [DataMember(Name = "FMExtID", IsRequired = true, EmitDefaultValue = false, Order = 4)]
+        public string? FmExtId { get; set; }
+
+        [DataMember(IsRequired = true, Order = 5)]
+        public int Sequence { get; set; }
+
+        [DataMember(IsRequired = true, Order = 6)]
+        public bool Fixed { get; set; }
+
+        [DataMember(IsRequired = true, Order = 7)]
+        public int State { get; set; }
+
+        [DataMember(IsRequired = true, Order = 8)]
+        public int DrivingTime { get; set; }
+
+        [DataMember(IsRequired = true, Order = 9)]
+        public int Distance { get; set; }
+
+        [DataMember(IsRequired = true, EmitDefaultValue = false, Order = 10)]
+        public string? Info { get; set; }
+
+        [DataMember(IsRequired = true, Order = 11)]
+        public int BuendelNr { get; set; }
+    }
+}
